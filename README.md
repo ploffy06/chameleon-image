5d3c46685c15c5981a2a77771d5496d3450c5168
Alice <alicewan0606@gmail.com>
2023-03-21 11:44:25 +1100

    MVP

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # chameleon-image
+Given a source image, `chameleon-image` will "merge" into a target image. The result is stored as a `.gif` file.
+
+This is done by training a model on a neural network, in which the model takes in the source image as the input. It learns via a MSE loss function where it computes the difference between the source and target image. Eventually, the model will learn to fit the source image to "become" the target image resulting in a sort of cool looking gif.
+
+# Results
+The following is the result of taking a dog as a source image and cat as the target image.
+
+<p align="center" >
+  <img src="result.gif" alt="animated" width="200" height="200"/>
+</p>
+
+# Running the script
+If you wish to run the script with your own input images simply change the the first paramter in the following lines.
+
+```
+src = read_image('dog.jpeg', mode=torchvision.io.ImageReadMode.RGB).float()
+...
+tgt = read_image('cat.jpeg', mode=torchvision.io.ImageReadMode.RGB).float()
+```
+
+Please note that the `read_image` function can only take in either a `.jpeg` or `.png` file type. In addition, the `Resize()` transormation will skew that image to a given size. As I chose a size of `(64, 64)` it is reccomended that you either have square images or change the size variable.
---
+++ script.py
@@ -0,0 +1,87 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision.io import read_image
+import torchvision.transforms as transforms
+from PIL import Image
+
+# ----- image preprocessing -----
+img_transforms = transforms.Compose([
+    transforms.Resize((64, 64))
+    ])
+
+# Souce image here: this is the image we start with
+src = read_image('dog.jpeg', mode=torchvision.io.ImageReadMode.RGB).float()
+src = img_transforms(src)
+
+# Target image here: this is the image we with to "merge" into
+tgt = read_image('cat.jpeg', mode=torchvision.io.ImageReadMode.RGB).float()
+tgt = img_transforms(tgt)
+
+
+print(f"src: shape={src.shape} dtype={src.dtype}")
+print(f"tgt: shape={tgt.shape} dtype={tgt.dtype}")
+
+
+# ----- creating model -----
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm1d(32),
+            nn.ConvTranspose2d( 64, 3, 4, 2, 1, bias=False),
+            nn.BatchNorm1d(64)
+        )
+
+    def __call__(self, x):
+        out = self.model(x)
+
+        output_img = transforms.ToPILImage()(out.type(torch.uint8))
+
+        return output_img, out
+
+# ----- initial set up -----
+images = [ transforms.ToPILImage()(src.type(torch.uint8))]
+epoch = 50000
+model = Model()
+criterion = nn.MSELoss()
+
+lr = 1e-2
+
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+# ----- training -----
+for i in range(epoch):
+    optimizer.zero_grad()
+    output_img, out = model(src)
+
+    loss = criterion(out, tgt)
+
+    loss.backward()
+    optimizer.step()
+    # if loss < 1000: # loss seems to get stuck ~900s... just want to give it a little push
+    #     lr = 1
+    #     for param_group in optimizer.param_groups:
+    #         param_group['lr'] = lr
+
+    if i % 1000 == 0:
+        images.append(output_img)
+        print(f"epoch: {i}, loss: {loss}")
+
+# ----- creating a gif file -----
+def make_gif(images, filename):
+    """creates a gif file given an array of images
+
+    Args:
+        images (list of images): array of images of convert into gif
+        filename (string): a string for the filename
+    """
+    frames = images
+    frame_one = frames[0]
+    fname = filename + ".gif"
+    frame_one.save(fname, format="GIF", append_images=frames,
+               save_all=True, duration=100, loop=0)
+
+make_gif(images, "result")
